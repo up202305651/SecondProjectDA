31655a01f19072275b7bace40392078f15eb8445
up202305651 <up202305651@up.pt>
2025-05-23 16:17:39 +0100

    Update bruteforce.cpp

--- bruteforce.cpp
+++ bruteforce.cpp
@@ -7,46 +7,46 @@
 #include <vector>
 using namespace std;
 
-void bruteforce(Data& data,vector<Pallet>& pallets) {
+void bruteforce(Data& data, vector<Pallet>& pallets) {
     vector<Pallet> solucao;
-    int n=pallets.size(); //mesma coisa que usar a o parametro da data
+    int n = pallets.size(); // mesma coisa que usar o parâmetro da data
     int max_profit = 0;
     int best_mask = 0;
 
-    for (int mask=0; mask<(1<<n);++mask){
-        int cur_weight=0;
-        int cur_profit=0;
+    for (int mask = 0; mask < (1 << n); ++mask) {
+        int cur_weight = 0;
+        int cur_profit = 0;
 
-        for (int i=0; i<n;++i) {
-            if (mask & (1<<i)) {
-                cur_weight+=pallets[i].weight;
-                cur_profit+=pallets[i].profit;
+        for (int i = 0; i < n; ++i) {
+            if (mask & (1 << i)) {
+                cur_weight += pallets[i].weight;
+                cur_profit += pallets[i].profit;
             }
         }
-        if (cur_weight<=data.capacity && cur_profit>max_profit) {
-            max_profit=cur_profit;
-            best_mask=mask;
+        if (cur_weight <= data.capacity && cur_profit > max_profit) {
+            max_profit = cur_profit;
+            best_mask = mask;
         }
     }
 
-    //constrói a melhor solução
+    // constrói a melhor solução
     solucao.clear();
     for (int i = 0; i < n; ++i) {
         if (best_mask & (1 << i)) {
-            soluç.push_back(pallets[i]);
+            solucao.push_back(pallets[i]);
         }
     }
 
-    //output
+    // output
     int total_weight = 0;
     for (const auto& p : solucao)
         total_weight += p.weight;
 
     cout << "Brute Force Algorithm\n";
-    cout << "Lucro total: "<<max_profit<<"\n";
-    cout << "Peso total: "<<total_weight<<"\n";
+    cout << "Lucro total: " << max_profit << "\n";
+    cout << "Peso total: " << total_weight << "\n";
     cout << "Pallets selecionados: ";
     for (const auto& p : solucao)
-        cout<<p.id<<" ";
+        cout << p.id << " ";
     cout << "\n\n";
 }
